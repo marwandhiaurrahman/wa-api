27afa0681b36c016e4de10e791e26fb5aafd3dad
marwandhiaurrahman@gmail.com <marwandhiaurrahman@gmail.com>
2025-05-07 16:13:25 +0700

    error pupetter

--- multi.js
+++ multi.js
@@ -57,12 +57,6 @@ const createSession = function (id, description, webhook) {
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process', // <- this one doesn't works in Windows
-        '--disable-gpu'
       ],
     },
     authStrategy: new LocalAuth({
